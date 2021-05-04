f5a8d9705a62e2bc7e6d0c4a6f5620bade95b670
Kerr Salabao <kerr712@gmail.com>
2021-05-04 10:06:12 -0500

    Added input name that returns the state

--- connect.php
+++ connect.php
@@ -1,17 +1,22 @@
 <?php
 $mysqli = new mysqli("mysql.eecs.ku.edu", "kerr712", "basohR7h", "kerr712");
 
+
+$name = $_POST["name"];
+
+
 /* check connection */
 if ($mysqli->connect_errno) {
     printf("Connect failed: %s\n", $mysqli->connect_error);
     exit();
 }
 
-$query = "SELECT CRUISENUM FROM CRUISE";
+
+$query = "SELECT * FROM CITY WHERE CITY_NAME = '" . $name . "'";
 if ($result = $mysqli->query($query)) {
      /* fetch associative array */
      while ($row = $result->fetch_assoc()) {
-        echo $row["CRUISENUM"] . "<br>";
+        echo $row["STATE"] . "<br>";
     }
 
 
